a79d704c96a549d29f985ed94691b7428b76108e
maxgreedishim <90276401+maxgreedishim@users.noreply.github.com>
2021-10-08 19:04:36 +0500

    NumberHolder просто

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,15 @@ public class Main {
     public static class NumberHolder {
         public static int anInt;
         public static float aFloat;
-        public static int anIntOne;
-        public static float aFloatOne;
 
-        public NumberHolder(int anIntOne, float aFloatOne) {
-            this.anIntOne = anIntOne;
-            this.aFloatOne = aFloatOne;
-        }
     }
+
     public static void main(String[] args) {
-        new NumberHolder(23,23.23f);
+        NumberHolder numberHolder = new NumberHolder();
         NumberHolder.aFloat = 1.2f;
         NumberHolder.anInt = 12;
-        System.out.println(NumberHolder.anInt);
-        System.out.println(NumberHolder.aFloat);
-        System.out.println(NumberHolder.aFloatOne);
-        System.out.println(NumberHolder.anIntOne);
+        System.out.println(numberHolder.anInt);
+        System.out.println(numberHolder.aFloat);
+
     }
 }
