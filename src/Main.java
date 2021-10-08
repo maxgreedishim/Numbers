bae0f9743347eee20c746a3c49782aeaadafac65
maxgreedishim <90276401+maxgreedishim@users.noreply.github.com>
2021-10-08 22:16:11 +0500

    NumberHolder правка

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static class NumberHolder {
-        public static int anInt;
-        public static float aFloat;
+        public int anInt;
+        public float aFloat;
 
     }
 
     public static void main(String[] args) {
         NumberHolder numberHolder = new NumberHolder();
-        NumberHolder.aFloat = 1.2f;
-        NumberHolder.anInt = 12;
+        numberHolder.aFloat = 1.2f;
+        numberHolder.anInt = 12;
+        System.out.println(numberHolder.anInt);
+        System.out.println(numberHolder.aFloat);
+        numberHolder = new NumberHolder();
+        numberHolder.aFloat = 1.8f;
+        numberHolder.anInt = 19;
         System.out.println(numberHolder.anInt);
         System.out.println(numberHolder.aFloat);
 
